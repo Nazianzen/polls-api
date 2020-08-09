6935ea3497bd4dfd024fba9eaad3b9b148d4c6d8
Gideon Anyalewechi <ganyalewechi1997@gmail.com>
2020-08-09 21:21:56 +0100

    View implementation with using APIView

--- src/polls/urls.py
+++ src/polls/urls.py
@@ -1,4 +1,10 @@
 from django.urls import path
+from polls.views import (
+    PollList,
+    PollDetail,
+)
 
 urlpatterns = [
+    path('polls/', PollList.as_view(), name='polls-list'),
+    path('polls/<int:pk>/', PollDetail.as_view(), name='polls-detail'),
 ]
