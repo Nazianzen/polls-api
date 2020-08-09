f0ff01242b9777ded0841502d0f3267dcffc4b8f
Gideon Anyalewechi <ganyalewechi1997@gmail.com>
2020-08-09 20:40:16 +0100

    Basic api implementation using pure django

--- src/polls/urls.py
+++ src/polls/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
+from polls import views
 
 urlpatterns = [
+    path('polls/', views.polls_list, name='polls-list'),
+    path('polls/<int:pk>/', views.polls_detail, name='polls_detail'),
 ]
